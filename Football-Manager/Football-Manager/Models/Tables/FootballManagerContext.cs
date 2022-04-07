e17cc9893a463821639a04d4ab2c9cc09129ed61
mGaud <michaelgaud991@gmail.com>
2022-04-07 19:18:38 +0200

    setup entity framework

---
+++ Football-Manager/Football-Manager/Models/Tables/FootballManagerContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Football_Manager.Models.Tables
+{
+    public class FootballManagerContext : DbContext
+    {
+        public FootballManagerContext(DbContextOptions context) : base(context)
+        {
+
+        }
+    }
+}
--- Football-Manager/Football-Manager/Program.cs
+++ Football-Manager/Football-Manager/Program.cs
@@ -1,13 +1,17 @@
+using Football_Manager.Models.Tables;
+using  Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<FootballManagerContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("FootBallManagerDatabase")));
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
